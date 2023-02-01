dabe08fbe8415fcae8846c1edfea1b578654f2ed
minawoien <mc.woien@stud.uis.no>
2023-02-01 14:46:21 +0100

    change folder structure

---
+++ pdffunc.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def norm1D(my, Sgm, x):
+    [n, d] = np.shape(x)
+    p = np.zeros(np.shape(x))
+    for i in np.arange(0, n):
+        p[i] = 1 / (np.sqrt(2 * np.pi) * Sgm) * \
+            np.exp(-1 / 2 * np.square((x[i] - my)) / (np.square(Sgm)))
+
+    return p
+
+
+def norm_2D(my, Smg, x_1, x_2):
+    [n1, d1] = np.shape(x_1)
+    p = np.zeros_like(x_1)
+    for i in np.arange(0, n1):
+        for j in np.arange(0, d1):
+            x = np.array([[x_1[i, j]], [x_2[i, j]]])
+            p[i, j] = 1 / ((2*np.pi)**(2/2)*(np.linalg.det(Smg))
+                           ** (1/2)) * np.exp(-1/2*np.transpose(x - my)@np.linalg.inv(Smg)@(x-my))
+    return p
--- pltfunc.py
+++ pltfunc.py
@@ -1,17 +1,5 @@
-import sys
-import numpy as np
 import matplotlib.pyplot as plt
-
-
-def norm_2D(my, Smg, x_1, x_2):
-    [n1, d1] = np.shape(x_1)
-    p = np.zeros_like(x_1)
-    for i in np.arange(0, n1):
-        for j in np.arange(0, d1):
-            x = np.array([[x_1[i, j]], [x_2[i, j]]])
-            p[i, j] = 1 / ((2*np.pi)**(2/2)*(np.linalg.det(Smg))
-                           ** (1/2)) * np.exp(-1/2*np.transpose(x - my)@np.linalg.inv(Smg)@(x-my))
-    return p
+import numpy as np
 
 
 def plot(p, x_1, x_2, col, db):
